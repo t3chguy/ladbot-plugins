2f50bed04bccad4d415171e3db0cfbf04d0f4ba0
Michael Telatynski <7t3chguy@gmail.com>
2015-04-20 19:45:27 +0100

    Lads should only function for Channels!

--- lads/index.js
+++ lads/index.js
@@ -6,17 +6,21 @@ module.exports =
 	{
 		"lads": function(msg, sender, api, chan)
 		{
-			var names = api.getNames(chan);
-			var str = "";
+			if (chan === 0) {
+				api.say("This is a Private Conversation. No Pervs Here.");
+			} else {
+				var names = api.getNames(chan);
+				var str = "";
 
-			var i;
-			for (i in names)
-			{
-				if (i !== api.conf.nick && i !== sender)
-					str += i+" ";
+				var i;
+				for (i in names)
+				{
+					if (i !== api.conf.nick && i !== sender)
+						str += i+" ";
+				}
+
+				api.say(str);
 			}
-
-			api.say(str);
 		}
 	}
 }
